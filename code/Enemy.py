d55f42b3c66715e3d494bd08fb0fe9c62482bea9
Luis <luisfernando000cruzdesousa@gmail.com>
2024-11-04 13:24:19 -0300

    Update and bug fixes

--- code/Enemy.py
+++ code/Enemy.py
@@ -9,23 +9,23 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
-        self.vertical_direction = -1  # -1 para subir, 1 para descer
-        self.speed_y = ENTITY_SPEED[self.name]  # Velocidade inicial vertical
+        self.vertical_direction = -1
+        self.speed_y = ENTITY_SPEED[self.name]
 
     def move(self):
             self.rect.centerx -= ENTITY_SPEED[self.name]
-            # Movimento vertical com lógica especial para Enemy3
+            # Vertical movement with special logic for Enemy3
             if self.name == 'Enemy3':
-                # Atualiza a posição vertical com a velocidade atual
+                # Updates the vertical position with the current speed
                 self.rect.centery += self.speed_y * self.vertical_direction
 
-                # Verifica colisão com as bordas superior e inferior
+                # Check for collision with top and bottom edges
                 if self.rect.top <= 0:
-                    # Ao atingir a borda superior, desce com o dobro da velocidade
+                    # When it reaches the top edge, it descends twice as fast
                     self.vertical_direction = 1
                     self.speed_y = ENTITY_SPEED[self.name] * 2
                 elif self.rect.bottom >= WIN_HEIGHT:
-                    # Ao atingir a borda inferior, sobe com a velocidade normal
+                    # Upon reaching the lower edge, it rises with normal speed
                     self.vertical_direction = -1
                     self.speed_y = ENTITY_SPEED[self.name]
 
